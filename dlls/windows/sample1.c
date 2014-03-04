fd145d013ea0743e0a1f99266f8aff25479d735a
Taku Nakajima <tnaka@dc4.so-net.ne.jp>
2014-03-04 15:15:57 +0900

    test cli on windows

---
+++ dlls/windows/sample1.c
@@ -0,0 +1,19 @@
+
+#include "sample1.h"
+
+
+int api1(int a, int b, struct Sample *out) {
+    int i;
+    char *p;
+    out->sum = a + b;
+    out->difference = a - b;
+    for(i = 0 ; i< 16 ; i++) {
+        out->platform[i] = 0;
+    }
+    p = "windows";
+    i = 0;
+    while(*p != 0) {
+        out->platform[i++] = *p++;
+    }
+    return 1;
+}
---
+++ dlls/windows/sample1.h
@@ -0,0 +1,9 @@
+
+struct Sample {
+    int sum;
+    int difference;
+    char platform[16];
+};
+
+int api1(int a, int b, struct Sample *out);
+
